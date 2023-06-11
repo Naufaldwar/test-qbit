31c5da712d93d884db149978b01ff4376a45cc26
naufaldwar <naufaldwiariananto@gmail.com>
2023-06-11 22:26:46 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import { fruits, comments } from "./data.ts";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [fruitTypeCount, setFruitTypeCount] = useState(0);
+  useEffect(() => {
+    const uniqueFruitTypes = new Set(fruits.map((fruit) => fruit.fruitType));
+    const count = uniqueFruitTypes.size;
+    setFruitTypeCount(count);
+  }, []);
+
+  const countCommentContent = (comments) => {
+    let count = 0;
+
+    for (const comment of comments) {
+      count++;
+
+      if (comment.replies) {
+        count += countCommentContent(comment.replies);
+      }
+    }
+
+    return count;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Case 1</h1>
+      <p>
+        buah yang dimiliki andi :{" "}
+        {fruits.map((item, index) => (
+          <span style={{ marginLeft: "4px" }} key={index}>
+            {item.fruitName}
+          </span>
+        ))}
+      </p>
+      <p>jumlah wadah: {fruitTypeCount}</p>
+      <p>
+        buah local:{" "}
+        {fruits.map((item, index) => {
+          if (item.fruitType === "LOCAL") {
+            return (
+              <span key={index} style={{ marginLeft: "4px" }}>
+                {item.fruitName}
+              </span>
+            );
+          }
+        })}
+      </p>
+
+      <p>
+        buah import:
+        {fruits.map((item, index) => {
+          if (item.fruitType === "IMPORT") {
+            return (
+              <span key={index} style={{ marginLeft: "4px" }}>
+                {item.fruitName}
+              </span>
+            );
+          }
+        })}
+      </p>
+      <p>komentar : terdapat id yang sama</p>
+      <h1>Case 2 </h1>
+      <p>Jumlah Comment : {countCommentContent(comments)}</p>
     </div>
   );
 }
---
+++ src/data.ts
@@ -0,0 +1,95 @@
+type IFruit = {
+  fruitId: number;
+  fruitName: string;
+  fruitType: "IMPORT" | "LOCAL";
+  stock: number;
+};
+
+const fruits: IFruit[] = [
+  {
+    fruitId: 1,
+    fruitName: "Apel",
+    fruitType: "IMPORT",
+    stock: 10,
+  },
+  {
+    fruitId: 2,
+    fruitName: "Kurma",
+    fruitType: "IMPORT",
+    stock: 20,
+  },
+  {
+    fruitId: 3,
+    fruitName: "apel",
+    fruitType: "IMPORT",
+    stock: 50,
+  },
+  {
+    fruitId: 4,
+    fruitName: "Manggis",
+    fruitType: "LOCAL",
+    stock: 100,
+  },
+  {
+    fruitId: 5,
+    fruitName: "Jeruk Bali",
+    fruitType: "LOCAL",
+    stock: 10,
+  },
+  {
+    fruitId: 6,
+    fruitName: "KURMA",
+    fruitType: "IMPORT",
+    stock: 20,
+  },
+  {
+    fruitId: 7,
+    fruitName: "Salak",
+    fruitType: "LOCAL",
+    stock: 150,
+  },
+];
+
+type IComment = {
+  commentId: number;
+  commentContent: string;
+  replies?: IComment[];
+};
+const comments: IComment[] = [
+  {
+    commentId: 1,
+    commentContent: "Hai",
+    replies: [
+      {
+        commentId: 11,
+        commentContent: "Hai juga",
+        replies: [
+          {
+            commentId: 111,
+            commentContent: "Haai juga hai jugaa",
+          },
+          {
+            commentId: 112,
+            commentContent: "Haai juga hai jugaa",
+          },
+        ],
+      },
+      {
+        commentId: 12,
+        commentContent: "Hai juga",
+        replies: [
+          {
+            commentId: 121,
+            commentContent: "Haai juga hai jugaa",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    commentId: 2,
+    commentContent: "Halooo",
+  },
+];
+
+export { fruits, comments };
